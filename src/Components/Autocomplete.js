8e252d63c4218fe8e912fe3b9a67a8083122e6dd
Workbeesdig <adlen025@gmail.com>
2018-03-27 17:16:38 +0100

    Added Query from github search api

--- src/Components/Autocomplete.js
+++ src/Components/Autocomplete.js
@@ -4,13 +4,91 @@ import React from 'react';
  * A react component that displays an input with autocomplete functionnality
  */
 
-export class Autocomplete extends React.Component{
+export class Autocomplete extends React.Component {
+  constructor(props) {
+    super(props);
+    /**
+     * Initialize the search state
+     */
+    this.state = {
+      search: '',
+      items: [],
+      error: false
+    };
+  }
 
+  /**
+   * Get input value each time the input is search is changed, and update the state.
+   * @param {Event} e
+   */
+  onChange(e) {
+    // input value
+    let search = e.target.value;
 
-
-
-    render(){
-        return (<h3>Autocomplete</h3>);
-    }
+    this.fetchGithubData(search);
+  }
+  /**
+   * Fetch autocomplete data from Github's API to query repositories
+   * @param {String} search
+   */
+  queryGithubData(search) {
+      /**
+       * Query the github API
+       * 
+       * @type Promise
+       * @returns result or error
+       */
+    fetch(
+      'https://api.github.com/search/repositories?q=' +
+        search +
+        '&sort=stars&order=desc'
+    )
+        /**
+         * Parsing result object to json
+         */
+      .then(res => res.json())
+      .then(
+        result => {
+          if (result.items) {
+            /**
+             * Limit the results to 8 items and return only their names
+             */
+            let res = result.items.splice(0, 8).map(e => {
+              return e.name;
+            });
+            /**
+             * add results to state
+             */
+            this.setState({
+              
+              items: res
+            });
+          }
+        },
+        error => {
+          console.log(error);
+          this.setState({
+            error
+          });
+        }
+      );
+  }
+ 
+  /**
+   * Render the input
+   */
+  render() {
+    return (
+      <div className="input-field col s6">
+        <input
+          placeholder="Type something"
+          id="search"
+          type="text"
+          className="validate"
+          onChange={this.onChange.bind(this)}
+        />
+      </div>
+    );
+  }
 }
 
