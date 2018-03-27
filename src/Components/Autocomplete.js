ffb3e7a8a40c4de98dc57761edcc7edb730d372e
Workbeesdig <adlen025@gmail.com>
2018-03-27 16:09:13 +0100

    Create Autocomplete component

---
+++ src/Components/Autocomplete.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+/**
+ * A react component that displays an input with autocomplete functionnality
+ */
+
+export class Autocomplete extends React.Component{
+
+
+
+
+    render(){
+        return (<h3>Autocomplete</h3>);
+    }
+}
+
